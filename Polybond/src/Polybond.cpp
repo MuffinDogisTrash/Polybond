6c6a78d6b12ecf1550487e96927eae22ceefcb5e
legacyofducks <33140437+legacyofducks@users.noreply.github.com>
2017-10-26 19:18:22 -0700

    project
    Contains the source files.

---
+++ Polybond/src/Polybond.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Polybond.cpp
+// Author      : legacyofducks
+// Version     : 0.0.1
+// Copyright   : © 2017 polybondgame.com ALL RIGHTS RESERVED
+// Description : A really cool game
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Welcome to the POLYBOND Console." << endl; // prints Welcome to the POLYBOND Console.
+	return 0;
+}
